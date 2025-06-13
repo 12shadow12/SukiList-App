eca701e72c7440347d98a53e60844cbc46cbf1c6
Rich Bui <rbui54321@gmail.com>
2025-06-12 23:02:24 -0700

    changed vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const isProduction = mode === 'production'
-
-  return {
-    plugins: [react()],
-    base: isProduction ? '/SukiList-App/' : '/',
-  }
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
 })
